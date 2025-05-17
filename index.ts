5a97908857171950228609e72116a4db8513a2a5
Christian max <christ@deptechdigital.id>
2025-05-17 23:21:18 +0700

    update hono server

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
 // import { serve } from "bun";
 import { serveStatic } from "hono/serve-static";
-import app from "server";
 import path from 'path'
 import fs from 'fs/promises'
+import { Hono } from 'hono';
+import trpcHandler from './server/routes/trpc';
+import { cors } from 'hono/cors';
+
+const app = new Hono();
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://b2bapp-maxchris100s-projects.vercel.app'],  // Allow requests from frontend
+    allowMethods: ['GET', 'POST', 'OPTIONS'],  // Make sure these methods are allowed
+    allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Add more if needed
+}));
+
+
+app.use('/trpc/*', async (c, next) => {
+    const url = new URL(c.req.url);
+    if (url.pathname.startsWith('/trpc')) {
+        return trpcHandler(c.req.raw);
+    }
+    return next();
+});
 
 // Serve static assets dari folder client/dist/assets
 app.use('/assets/*', serveStatic({
--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,24 @@
-import { Hono } from 'hono';
-import trpcHandler from './routes/trpc';
-import { cors } from 'hono/cors';
+// import { Hono } from 'hono';
+// import trpcHandler from './routes/trpc';
+// import { cors } from 'hono/cors';
 
-const app = new Hono();
-
-
-app.use(cors({
-    origin: 'http://localhost:3000',  // Allow requests from frontend
-    allowMethods: ['GET', 'POST', 'OPTIONS'],  // Make sure these methods are allowed
-    allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Add more if needed
-}));
+// const app = new Hono();
 
 
-app.use('/trpc/*', async (c, next) => {
-    const url = new URL(c.req.url);
-    if (url.pathname.startsWith('/trpc')) {
-        return trpcHandler(c.req.raw);
-    }
-    return next();
-});
+// app.use(cors({
+//     origin: 'http://localhost:3000',  // Allow requests from frontend
+//     allowMethods: ['GET', 'POST', 'OPTIONS'],  // Make sure these methods are allowed
+//     allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Add more if needed
+// }));
 
-export default app;
+
+// app.use('/trpc/*', async (c, next) => {
+//     const url = new URL(c.req.url);
+//     if (url.pathname.startsWith('/trpc')) {
+//         return trpcHandler(c.req.raw);
+//     }
+//     return next();
+// });
+
+// export default app;
 
