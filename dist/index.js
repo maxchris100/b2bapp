d6e0b5ae3282b38eb5b57a99e6139ef32e300832
Christian max <christ@deptechdigital.id>
2025-05-18 20:35:01 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+import { serve } from '@hono/node-server';
 import { serveStatic } from '@hono/node-server/serve-static';
 import { handle } from '@hono/node-server/vercel';
 import { Hono } from 'hono';
@@ -10,6 +11,10 @@ app.use(cors({
     allowMethods: ['GET', 'POST', 'OPTIONS'], // Make sure these methods are allowed
     allowHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Add more if needed
 }));
+app.use('*', async (c, next) => {
+    console.log(`[${new Date().toISOString()}] ${c.req.method} ${c.req.url}`);
+    return next();
+});
 app.use('/api/*', trpcServer({ router: appRouter }));
 app.use('/assets/*', serveStatic({ root: './client/dist' })); // serve CSS/JS
 // Serve all static files
@@ -17,7 +22,7 @@ app.use('/*', serveStatic({ root: './client/dist' }));
 // SPA fallback ke index.html
 app.use('*', serveStatic({ path: './client/dist/index.html' }));
 // if (process.env.NODE_ENV !== 'vercel') {
-// console.log('🚀 Server is running on http://localhost:5000');
-// serve({ fetch: app.fetch, port: 5000 }); // 👈 di sini kamu set port-nya
+console.log('🚀 Server is running on http://localhost:5000');
+serve({ fetch: app.fetch, port: 5000 }); // 👈 di sini kamu set port-nya
 // }
 export default handle(app); // Ensure app is exported as the default export
